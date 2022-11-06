6ad3c39fd1bcb886b33e434973f87c6735efdd26
bluecrow <tachikoma.ghost@gmail.com>
2022-11-06 09:52:02 +0900

    Applicationを継承したクラスを追加

---
+++ app/src/main/java/jp/arkhamsoft/sample/viewmodel1/app/MyApplication.kt
@@ -0,0 +1,9 @@
+package jp.arkhamsoft.sample.viewmodel1.app
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
